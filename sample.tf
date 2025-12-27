44aca48070f2ef6b4aea909011cf9765ff3919ce
suraj <surajbele51@gmail.com>
2025-12-27 09:12:39 +0530

    output block added

--- sample.tf
+++ sample.tf
@@ -30,7 +30,7 @@ variable "key_name" {
 }
 
 output "instance_public_ip" {
-  value = "aws_instance.myinstance.public_ip"
+  value = aws_instance.myinstance.public_ip
 }
 output "instance_id" {
   value = aws_instance.myinstance.id
