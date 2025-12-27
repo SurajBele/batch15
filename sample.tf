5cb490d933ba222f592e863fb0d58a02fa891919
suraj <surajbele51@gmail.com>
2025-12-27 08:57:52 +0530

    variables added

--- sample.tf
+++ sample.tf
@@ -9,12 +9,22 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "myinstance" {
-  ami = "ami-0c398cb65a93047f2"
-  instance_type = "t3.micro"
+  ami = var.ami_id
+  instance_type = var.instance_type
   vpc_security_group_ids = ["sg-0f7b2e9cf35734448"]
-  key_name = "id_rsa"
+  key_name = var.key_name
   availability_zone = "us-east-1a"
   tags = {
     name = "myinstance"
   }
 } 
+
+variable "ami_id" {
+  default = "ami-0c398cb65a93047f2"
+}
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "key_name" {
+  default = "id_rsa"
+}
