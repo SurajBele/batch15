846d80f0837a9c987adf7a9d9285d4e170cf535a
suraj <surajbele51@gmail.com>
2026-02-20 10:11:36 +0530

    testing stage added

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -17,6 +17,11 @@ pipeline {
         }
         stage('Test') {
             steps {
+              sh '''/opt/apache-maven-3.9.12/bin/mvn clean verify sonar:sonar \\
+                    -Dsonar.projectKey=myproject \\
+                    -Dsonar.host.url=http://43.205.98.76:9000 \\
+                    -Dsonar.login=sqp_7c6db7527fe810de171368b8c2dad0d7e038cd6d
+'''
                 echo "testing success"
             }
         }
