6110bd082a9101eb97296939e01bcdef79a28531
suraj <surajbele51@gmail.com>
2026-02-20 10:31:27 +0530

    testing sonar env stage added

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -17,11 +17,10 @@ pipeline {
         }
         stage('Test') {
             steps {
-              sh '''/opt/apache-maven-3.9.12/bin/mvn clean verify sonar:sonar \\
-                    -Dsonar.projectKey=myproject \\
-                    -Dsonar.host.url=http://43.205.98.76:9000 \\
-                    -Dsonar.login=sqp_7c6db7527fe810de171368b8c2dad0d7e038cd6d
-'''
+                withSonarQubeEnv( installationName: 'sonar-server', credentialsId: 'sonar-token') {
+                sh '/opt/apache-maven-3.9.12/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=myproject'
+}
+              
                 echo "testing success"
             }
         }
